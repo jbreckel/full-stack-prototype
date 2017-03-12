1f435386443a0cee28fb03df8a0eccce97466856
Julius Breckel <julius.breckel@signavio.com>
2017-03-12 15:50:46 +0300

    update mocks to work with react

--- server/graphql/schema/index.js
+++ server/graphql/schema/index.js
@@ -6,16 +6,29 @@ const schema = makeExecutableSchema({
   typeDefs,
 })
 
+const mocks = {
+  String: (x, y, z, { parentType, fieldName }) => `${parentType}-${fieldName}`,
+}
+
+const testMocks = {
+  ...mocks,
+  ID: (x, y, z, { parentType, fieldName }) => `${parentType}-${fieldName}`,
+  Boolean: () => true,
+  Int: () => 1,
+  Float: () => 1.5,
+}
+
+const serverMocks = {
+  ...mocks,
+  Int: () => Math.abs(Math.round(Math.random() * 200) - 100),
+  Float: () => Math.abs((Math.random() * 200) - 100).toFixed(2),
+  Boolean: () => Math.random() > 0.5,
+}
+
 addMockFunctionsToSchema({
   schema,
   // added static mock values, to allow snapshot testing of query results
-  mocks: {
-    Int: () => 1,
-    Float: () => 1.5,
-    String: (x, y, z, { fieldName }) => fieldName,
-    Boolean: () => true,
-    ID: (x, y, z, { fieldName }) => `id-${fieldName}`,
-  },
+  mocks: process.env.NODE_ENV === 'test' ? testMocks : serverMocks,
 })
 
 export default schema
