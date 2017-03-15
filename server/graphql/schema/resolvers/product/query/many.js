cc91272609311e21f7ce5603d5c26a0da2b62ee0
Julius Breckel <julius.breckel@signavio.com>
2017-03-16 00:06:30 +0300

    add selection to enable filter on category

--- server/graphql/schema/resolvers/product/query/many.js
+++ server/graphql/schema/resolvers/product/query/many.js
@@ -8,6 +8,11 @@ export default (root, { category }, ctx, options) =>
       path: 'category',
       match: { _id: category },
     })
+    .select({
+      ...getProjection(options.fieldNodes[0]),
+      // always select category to allow filter even when not requested in graphql
+      category: 1,
+    })
     .exec()
     // no filter when category is not specified
     .then((res) => (!category ? res : res.filter(({ category: c }) => c)))
