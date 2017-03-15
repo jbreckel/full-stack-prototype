02def93b30e9c31315683d9be1029b8859ec6f6c
Julius Breckel <julius.breckel@signavio.com>
2017-03-16 00:06:14 +0300

    add attribution

--- server/graphql/schema/utils/get-projection.js
+++ server/graphql/schema/utils/get-projection.js
@@ -1,3 +1,5 @@
+// from https://github.com/sitepoint-editors/graphql-nodejs/blob/master/graphql/get-projection.js
+
 export default (fieldASTs) =>
   fieldASTs.selectionSet.selections.reduce((projections, selection) => ({
     ...projections,
