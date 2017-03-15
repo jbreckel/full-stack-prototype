57fac7003246c7b9e5e1daa9e65ede77656ba8c5
Julius Breckel <julius.breckel@signavio.com>
2017-03-16 00:16:25 +0300

    let products take array of categories

--- server/graphql/schema/resolvers/product/query/many.js
+++ server/graphql/schema/resolvers/product/query/many.js
@@ -1,18 +1,17 @@
 import Product from '../../../models/product'
 import getProjection from '../../../utils/get-projection'
 
-export default (root, { category }, ctx, options) =>
-  Product
+export default (root, { categories = [] }, ctx, options) => {
+  const query = Product
     .find({})
-    .populate({
-      path: 'category',
-      match: { _id: category },
-    })
-    .select({
-      ...getProjection(options.fieldNodes[0]),
-      // always select category to allow filter even when not requested in graphql
-      category: 1,
-    })
+    .populate({ path: 'category' })
+    .select(getProjection(options.fieldNodes[0]))
+
+  if (categories.length > 0) {
+    query
+      .where('category')
+      .in(categories)
+  }
+  return query
     .exec()
-    // no filter when category is not specified
-    .then((res) => (!category ? res : res.filter(({ category: c }) => c)))
+}
