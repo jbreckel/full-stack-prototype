87d9f6df85f4a23d5ec621d6d076ae1b488cda74
Julius Breckel <julius.breckel@signavio.com>
2017-03-15 23:54:00 +0300

    add apollo to eslint-graphql

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,18 +7,32 @@ module.exports = {
     'import/no-extraneous-dependencies': [ 'error', {
       devDependencies: ['webpack.config.**', '**/test/**', '**/tools/**'],
     }],
-    'graphql/template-strings': ['error', {
-      env: 'literal',
-      schemaJson: require('./generated/schema.json'),
-      validators: without(specifiedRules.map(({ name }) => name),
-        // imports of fragments does not work in this env
-        'KnownFragmentNames',
-        // fragments will be imported and used in queries
-        'NoUnusedFragments',
-        // variables that are used inside fragments are not recognized
-        'NoUnusedVariables'
-      ),
-    }],
+    'graphql/template-strings': ['error',
+      {
+        env: 'literal',
+        schemaJson: require('./generated/schema.json'),
+        validators: without(specifiedRules.map(({ name }) => name),
+          // imports of fragments does not work in this env
+          'KnownFragmentNames',
+          // fragments will be imported and used in queries
+          'NoUnusedFragments',
+          // variables that are used inside fragments are not recognized
+          'NoUnusedVariables'
+        ),
+      },
+      {
+        env: 'apollo',
+        schemaJson: require('./generated/schema.json'),
+        validators: without(specifiedRules.map(({ name }) => name),
+          // imports of fragments does not work in this env
+          'KnownFragmentNames',
+          // fragments will be imported and used in queries
+          'NoUnusedFragments',
+          // variables that are used inside fragments are not recognized
+          'NoUnusedVariables'
+        ),
+      },
+    ],
     'react/prop-types': [0],
   },
   plugins: [
