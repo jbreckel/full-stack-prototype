72a7428db65657c688846e571f50bab4a3b1c9a0
Julius Breckel <julius.breckel@signavio.com>
2017-03-18 02:15:52 +0300

    update category filter in products/many

--- server/graphql/schema/resolvers/product/query/many.js
+++ server/graphql/schema/resolvers/product/query/many.js
@@ -3,16 +3,20 @@ import infoToProjection from 'graphql-mongodb-projection'
 import Product from '../../../models/product'
 
 export default (root, { categories = [] }, ctx, options) => {
-  console.log('select', infoToProjection(options))
   const query = Product
     .find({})
     .populate({ path: 'category' })
     .select(infoToProjection(options))
 
   if (categories.length > 0) {
+    const categoriesWithoutNull = categories.filter((c) => c != null)
     query
-      .where('category')
-      .in(categories)
+      .or(
+        [
+          categories.indexOf(null) !== -1 && { category: null },
+          categoriesWithoutNull.length > 0 && { category: { $in: categoriesWithoutNull } },
+        ].filter((c) => c)
+      )
   }
   return query
     .exec()
