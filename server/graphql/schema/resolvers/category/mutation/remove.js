ae129547a24a76ded08ee9cf02d90fd488a7b34b
Julius Breckel <julius.breckel@signavio.com>
2017-03-18 02:15:03 +0300

    update products on category delete

--- server/graphql/schema/resolvers/category/mutation/remove.js
+++ server/graphql/schema/resolvers/category/mutation/remove.js
@@ -2,9 +2,20 @@ import infoToProjection from 'graphql-mongodb-projection'
 
 import Category from '../../../models/category'
 
+import findProducts from '../../product/query/many'
+import updateProduct from '../../product/mutation/update'
+
 export default (root, { id }, ctx, options) =>
   Category
     .findByIdAndRemove(id, {
       select: infoToProjection(options),
     })
     .exec()
+    .then((doc) =>
+      findProducts(root, { categories: [id] }, ctx, options)
+        .then((products) =>
+          Promise.all(products.map(({ id: pid }) =>
+            updateProduct(root, { product: { id: pid, category: null } }, ctx, options)
+          )).then(() => doc)
+        )
+      )
